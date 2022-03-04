13070467167977edfc34c35148de47d113582159
Kamil Kerimov <koma_2002@bk.ru>
2022-03-05 00:46:33 +0500

    memory views offsets added

--- UMPK-80/src/views/StackView.cpp
+++ UMPK-80/src/views/StackView.cpp
@@ -14,5 +14,5 @@ StackView::~StackView()
 void StackView::Render(KR580VM80A* emu)
 {
 	if (m_MemoryEditor.Open)
-		m_MemoryEditor.DrawWindow("Stack", emu->Memory + STACK_OFFSET, STACK_SIZE);
+		m_MemoryEditor.DrawWindow("Stack", emu->Memory + STACK_OFFSET, STACK_SIZE, STACK_OFFSET);
 }
