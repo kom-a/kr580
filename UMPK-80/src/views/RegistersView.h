dd0655ee201069ae2b8bbb5886d592644c649e82
Kamil Kerimov <koma_2002@bk.ru>
2022-04-26 23:44:56 +0500

    Modifiable registers from Registers view

--- UMPK-80/src/views/RegistersView.h
+++ UMPK-80/src/views/RegistersView.h
@@ -11,4 +11,29 @@ public:
 	~RegistersView();
 
 	void Render(KR580VM80A* emu);
+private:
+	void DrawRegister(const char* label, WORD& reg);
+
+	struct State
+	{
+		WORD*	DataEditingAddr;
+		bool	DataEditingTakeFocus;
+		char	DataInputBuf[32];
+	};
+
+	struct Style
+	{
+		const char* FormatByte;
+	};
+
+	struct Sizes
+	{
+		float GlyphWidth;
+
+		Sizes() { memset(this, 0, sizeof(*this)); }
+	};
+
+	State m_State;
+	Style m_Style;
+	Sizes m_Sizes;
 };
